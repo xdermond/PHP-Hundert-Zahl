5852e90e3f8301eb7f50cee4aa5db1eae1b5d135
xdermond <ldurmeyer@gmail.com>
2025-10-22 12:20:40 +0200

    Hundert Zahl mit primm

--- index.php
+++ index.php
@@ -17,15 +17,53 @@
             padding: 40px;
             text-align: center;
         }
+        .prim
+        {
+            /*background-image: url("https://media0.faz.net/ppmedia/w1240/aktuell/1021900142/1.8543712/16x9/rainer-winkler-alias.jpg");
+            background-size: 170px;*/
+            background-color: yellow;
+        }
+        .yellow-box
+        {
+            width: 20px;
+            height: 20px;
+            background-color: yellow;
+        }
     </style>
+
+    
 
 
 </head>
 <body>
+    
+    <?php
+        function primzahl($zahl)
+        {
+            if ($zahl < 2) 
+            {
+                return false;
+            }
+            // Schleife von 2 bis zur Hälfte der Zahl
+            // (Optimierung: bis zur Quadratwurzel der Zahl reicht)
+            for ($i = 2; $i <= $zahl / 2; $i++) 
+            {
+            // Wenn die Zahl ohne Rest teilbar ist, ist sie keine Primzahl
+                if ($zahl % $i == 0) 
+                {
+                     return false;
+                }    
+            }  
+            
+            return true;
+
+        }
+    ?>
 
     <table>
             <tbody>
                 <?php
+                        $primmzahlzähler = 0;
 
                         for($i = 1; $i <= 100; $i++)
                         {
@@ -35,9 +73,16 @@
 
                             }   
 
-                            echo "<td>" ,$i, "</td>";
-
-
+                            if(primzahl($i) == true)
+                            {
+                                echo "<td class=\"prim\">", $i, "</td>";
+                                $primmzahlzähler ++;
+                            } 
+                            else
+                            {
+                                echo "<td>", $i, "</td>";
+                            }        
+                            
                             if($i % 10 == 0)
                             {
                                 echo"</tr>";
@@ -48,6 +93,17 @@
 
             </tbody>
     </table>
+
+    <br>
+    <br>
+
+     <div class = "yellow-box"></div>
+     <br>
+    
+    <?php
+        echo $primmzahlzähler, " Primzahlen";
+
+    ?>                    
 
 </body>
 </html>
